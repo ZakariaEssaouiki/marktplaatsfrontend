40aad8508f1664a5225cc0a1ef2ef4107eb047a4
Zakaria <z.essaouiki@ziggo.nl>
2022-12-21 10:36:53 +0100

    project id aangepast voor cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: 'as1zzv',
-  //projectId: '6sdrvq',
+  //projectId: 'as1zzv',
+  projectId: '6sdrvq',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
